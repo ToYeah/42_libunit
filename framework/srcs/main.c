ab5acede32facacea49d853c90281de6134c3dac
Taisei Toya <tytaisei0102@gmail.com>
2021-05-10 13:29:36 +0200

    feat(main.c): my_libftを削除

--- framework/srcs/main.c
+++ framework/srcs/main.c
@@ -89,35 +89,6 @@ const char* str_test_res(t_test_res res)
 		return "UNKNOWN";
 }
 
-size_t	my_strlen(const char *str)
-{
-	size_t res;
-
-	res = 0;
-	while(str[res])
-		res++;
-	return (res);
-}
-
-
-char	*my_strdup(const char *str)
-{
-	size_t len;
-	char *res;
-
-	len = my_strlen(str);
-	res = malloc(sizeof(char) * (len + 1));
-	if (res == NULL)
-		return NULL;
-	len = 0;
-	while (str[len])
-	{
-		res[len] = str[len];
-		len++;
-	}
-	return res;
-}
-
 int launch_tests(t_unit_test **test)
 {
 	t_unit_test *target;
